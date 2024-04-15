b912a6c548504a6c0cbe3df540cfc392a3dfec27
MatisLabelle <mat48337@gmail.com>
2024-04-15 13:05:47 -0400

    whale-spritesheet

--- js/script.js
+++ js/script.js
@@ -8,14 +8,29 @@ window.addEventListener("scroll", () => {
   }, 100);
 });
 
+let send = document.querySelector(".message-send");
+let output = document.querySelector(".message-output");
+
+send.addEventListener("click", function () {
+  let input = document.querySelector(".message-input").value;
+
+  output.textContent = input;
+});
+
 gsap.registerPlugin(ScrollTrigger);
 
+/*
+gsap
+  .timeline()
+  .fromTo(".whale-container", { x: "0vw" }, { x: "20vw", duration: 20 });
+*/
 gsap
   .timeline({
     duration: 2,
     scrollTrigger: {
       markers: true,
-      trigger: "#chapitre2",
+      trigger: "#chapitre6",
     },
   })
-  .to(".whale-container", { x: "20vw" });
+  .fromTo(".bulle", { opacity: 0 }, { opacity: 1 })
+  .fromTo(".message-output", { opacity: 0 }, { opacity: 1 });
