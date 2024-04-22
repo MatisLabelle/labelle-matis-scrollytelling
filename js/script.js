ae626a7fb88ebfdb3d728c0f474d90d09dd8a5df
MatisLabelle <mat48337@gmail.com>
2024-04-22 11:17:07 -0400

    fini-2-2

--- js/script.js
+++ js/script.js
@@ -19,18 +19,30 @@ send.addEventListener("click", function () {
 
 gsap.registerPlugin(ScrollTrigger);
 
-/*
 gsap
   .timeline()
-  .fromTo(".whale-container", { x: "0vw" }, { x: "20vw", duration: 20 });
-*/
+  .fromTo(".whale-container", { x: "25vw" }, { x: "50vw", duration: 15 })
+  .to(".whale-container", { scale: 0, duration: 5 }, "-=10")
+  .to(".whale-container", { x: "80vw", duration: 5 }, "<");
+
 gsap
   .timeline({
     duration: 2,
     scrollTrigger: {
-      markers: true,
       trigger: "#chapitre6",
     },
   })
   .fromTo(".bulle", { opacity: 0 }, { opacity: 1 })
   .fromTo(".message-output", { opacity: 0 }, { opacity: 1 });
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#chapitre5",
+    },
+  })
+  .fromTo(".no1", { y: "100vh" }, { y: 0, duration: 3 })
+  .fromTo(".no2", { y: "100vh" }, { y: 0, duration: 3 }, "-=2")
+  .fromTo(".no3", { y: "100vh" }, { y: 0, duration: 3 }, "-=2")
+  .fromTo(".no5", { y: "100vh" }, { y: 0, duration: 3 }, "-=2")
+  .fromTo(".no4", { y: "100vh" }, { y: 0, duration: 3 }, "-=2");
